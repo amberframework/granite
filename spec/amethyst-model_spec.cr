fa258e0ab5a0806f768bca75c119de3580c490cf
dru.jensen <dru.jensen@reachlocal.com>
2015-08-22 00:51:41 -0700

    add some specs

--- spec/amethyst-model_spec.cr
+++ spec/amethyst-model_spec.cr
@@ -1,9 +1,66 @@
 require "./spec_helper"
 
 describe Amethyst::Model do
-  # TODO: Write tests
-
-  it "works" do
-    false.should eq(true)
+  Spec.before_each do
+    Post.clear
   end
+
+  describe "#all" do
+    it "should find all the posts" do
+      post = Post.new
+      post.name = "Test Post"
+      post.save
+      post = Post.new
+      post.name = "Test Post 2"
+      post.save
+      posts = Post.all
+      posts.size.should eq 2
+    end
+  end
+
+  describe "#find" do
+    it "should find the post by id" do
+      post = Post.new
+      post.name = "Test Post"
+      post.save
+      id = post.id
+      post = Post.find id
+      post.should_not be_nil 
+    end
+  end
+
+  describe "#save" do
+    it "should create a new post" do
+      post = Post.new
+      post.name = "Test Post"
+      post.body = "Test Post"
+      post.save
+      post.id.should eq 1
+    end
+
+    it "should update an existing post" do
+      post = Post.new
+      post.name = "Test Post"
+      post.save
+      post.name = "Test Post 2"
+      post.save
+      post = Post.find 1
+      if post
+        post.name.should eq "Test Post 2"
+      end
+    end
+  end
+
+  describe "#destroy" do
+    it "should destroy a post" do
+      post = Post.new
+      post.name = "Test Post"
+      post.save
+      id = post.id
+      post.destroy
+      post = Post.find id
+      post.should be_nil
+    end
+  end
+
 end
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,20 @@
 require "spec"
 require "../src/amethyst-model"
+include Amethyst::Model
+
+class Post < Base::Model
+  fields({ name: "VARCHAR(255)", body: "TEXT" })
+end
+
+module Base
+  class App
+    def self.settings
+      @@config ||= Config.new
+    end
+  end
+  class Config
+    property :environment
+  end
+end
+
+Base::App.settings.environment = "test"
