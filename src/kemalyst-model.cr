9baf4f5ed49f9ed29c9b6771a5b1d0b309628701
jwaldrip <jason@waldrip.net>
2016-09-26 22:17:41 -0600

    fix "as" for crystal 0.19.x

--- src/kemalyst-model.cr
+++ src/kemalyst-model.cr
@@ -9,7 +9,7 @@ class Kemalyst::Model
     @errors ||= [] of String
   end
 
-  # specify the database adapter you will be using for this model. 
+  # specify the database adapter you will be using for this model.
   # mysql, pg, sqlite, etc.
   macro adapter(name)
     def self.settings
@@ -22,7 +22,7 @@ class Kemalyst::Model
     def self.database
       @@database
     end
-  end  
+  end
 
   # sql_mapping is the mapping between columns in your database and the fields
   # in this model.  proerties will be created for each field.  The type of the
@@ -45,35 +45,35 @@ class Kemalyst::Model
     property created_at : Time?
     property updated_at : Time?
     {% end %}
-   
+
     # Create the from_sql method
     def self.from_sql(result)
       model = {{@type.name.id}}.new
-      
+
       # hack around different types for Pg and Mysql drivers
-      unless model.id = result[0] as? Int32
-        model.id = result[0] as? Int64
+      unless model.id = result[0].as?(Int32)
+        model.id = result[0].as?(Int64)
       end
       {% i = 1 %}
       {% for name, types in fields %}
         # Need to find a way to map to other types based on SQL type
         if result[{{i}}].class == Slice(UInt8) && {{types[1].id}} == String
-          model.{{name.id}} = String.new ( result[{{i}}] as Slice(UInt8) )
+          model.{{name.id}} = String.new ( result[{{i}}].as(Slice(UInt8)) )
         else
-          model.{{name.id}} = result[{{i}}] as? {{types[1].id}}
+          model.{{name.id}} = result[{{i}}].as?({{types[1].id}})
         end
         {% i += 1 %}
       {% end %}
 
       {% if timestamps %}
-        unless model.created_at = result[{{i}}] as? Time
-          created_at_string = result[{{i}}] as? String
+        unless model.created_at = result[{{i}}].as?(Time)
+          created_at_string = result[{{i}}].as?(String)
           if created_at_string
             model.created_at = Time::Format.new("%F %X").parse(created_at_string)
           end
         end
-        unless model.updated_at = result[{{i + 1}}] as? Time
-          updated_at_string = result[{{i + 1}}] as? String
+        unless model.updated_at = result[{{i + 1}}].as?(Time)
+          updated_at_string = result[{{i + 1}}].as?(String)
           if updated_at_string
             model.updated_at = Time::Format.new("%F %X").parse(updated_at_string)
           end
@@ -155,7 +155,7 @@ class Kemalyst::Model
   end
 
   # Perform a query directly against the database
-  def self.query(statement : String, params = {} of String => String) 
+  def self.query(statement : String, params = {} of String => String)
     if db = @@database
       results = db.query(statement, params, fields)
     end
@@ -205,22 +205,22 @@ class Kemalyst::Model
       return false
     end
   end
-  
+
   # All will return all rows in the database. The clause allows you to specify
   # a WHERE, JOIN, GROUP BY, ORDER BY and any other SQL92 compatible query to
   # your table.  The results will be an array of instantiated instances of
   # your Model class.  This allows you to take full advantage of the database
   # that you are using so you are not restricted or dummied down to support a
-  # DSL.  
+  # DSL.
   def self.all(clause = "", params = {} of String => String)
     return self.select(@@table_name, fields({"id" => "INT"}), clause, params)
   end
-  
+
   # find returns the row with the id specified.
   def self.find(id)
     return self.select_one(@@table_name, fields({"id" => "INT"}), id)
   end
-  
+
   # select performs the select statement and calls the from_sql with the
   # results.
   def self.select(table_name, fields, clause, params = {} of String => String)
