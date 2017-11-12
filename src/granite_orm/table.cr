c6d724934eb70ae12922bff5af8af3797bbbe6ec
Dru Jensen <drujensen@gmail.com>
2017-11-12 07:01:50 -0800

    only create adapter if its not been set

--- src/granite_orm/table.cr
+++ src/granite_orm/table.cr
@@ -8,7 +8,7 @@ module Granite::ORM::Table
   # specify the database adapter you will be using for this model.
   # mysql, pg, sqlite, etc.
   macro adapter(name)
-    @@adapter = Granite::Adapter::{{name.id.capitalize}}.new("{{name.id}}")
+    @@adapter ||= Granite::Adapter::{{name.id.capitalize}}.new("{{name.id}}")
 
     def self.adapter
       @@adapter
