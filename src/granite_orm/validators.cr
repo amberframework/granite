981f0932926d6d72ec83f8f3d931b08f1a06ea73
Faustino Aguilar <faustinoaq@users.noreply.github.com>
2018-01-22 12:02:49 -0500

    Change validators to instance vars

--- src/granite_orm/validators.cr
+++ src/granite_orm/validators.cr
@@ -1,32 +1,33 @@
 require "./error"
 
 module Granite::ORM::Validators
-  property errors = [] of Error
+  getter errors = [] of Error
 
   macro included
     macro inherited
-      @@validators = Array({field: Symbol, message: String, block: Proc(Bool)}).new
-      def validate!;end
+      @validators = Array({field: Symbol, message: String, block: Proc(Bool)}).new
+      def validate!
+      end
     end
   end
 
   macro validate(message)
     def validate!
       previous_def
-      @@validators << {field: :base, message: {{message}}, block: ->{{{yield}}}}
+      @validators << {field: :base, message: {{message}}, block: ->{{{yield}}}}
     end
   end
 
   macro validate(field, message)
     def validate!
       previous_def
-      @@validators << {field: {{field}}, message: {{message}}, block: ->{{{yield}}}}
+      @validators << {field: {{field}}, message: {{message}}, block: ->{{{yield}}}}
     end
   end
 
   def valid?
     validate!
-    @@validators.each do |validator|
+    @validators.each do |validator|
       unless validator[:block].call
         errors << Error.new(validator[:field], validator[:message])
       end
