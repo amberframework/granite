1865279ac6add12c66f317265293b97ecdba873c
Dru Jensen <drujensen@gmail.com>
2018-03-27 20:15:13 -0700

    fix commas

--- spec/spec_models.cr
+++ spec/spec_models.cr
@@ -10,20 +10,20 @@ end
     if adapter == "pg"
       primary_key_sql = "BIGSERIAL PRIMARY KEY".id
       foreign_key_sql = "BIGINT".id
-      created_at_sql = "created_at TIMESTAMP,".id
-      updated_at_sql = "updated_at TIMESTAMP,".id
+      created_at_sql = "created_at TIMESTAMP".id
+      updated_at_sql = "updated_at TIMESTAMP".id
       timestamp_fields = "timestamps".id
     elsif adapter == "mysql"
       primary_key_sql = "BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY".id
       foreign_key_sql = "BIGINT".id
-      created_at_sql = "created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,".id
-      updated_at_sql = "updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,".id
+      created_at_sql = "created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP".id
+      updated_at_sql = "updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP".id
       timestamp_fields = "timestamps".id
     elsif adapter == "sqlite"
       primary_key_sql = "INTEGER NOT NULL PRIMARY KEY".id
       foreign_key_sql = "INTEGER".id
-      created_at_sql = "created_at VARCHAR,".id
-      updated_at_sql = "updated_at VARCHAR,".id
+      created_at_sql = "created_at VARCHAR".id
+      updated_at_sql = "updated_at VARCHAR".id
       timestamp_fields = "timestamps".id
     end
   %}
@@ -49,9 +49,9 @@ end
         exec("DROP TABLE IF EXISTS #{ quoted_table_name };")
         exec("CREATE TABLE #{ quoted_table_name } (
           id {{ primary_key_sql }},
-          {{ created_at_sql }}
+          name VARCHAR(100),
+          {{ created_at_sql }},
           {{ updated_at_sql }}
-          name VARCHAR(100)
         );
         ")
       end
@@ -198,7 +198,7 @@ end
             sentiment FLOAT,
             interest REAL,
             published BOOL,
-            {{ created_at_sql }}
+            {{ created_at_sql }},
             {{ updated_at_sql }}
           )
         SQL
