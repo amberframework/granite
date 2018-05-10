f25c4532aea501186db800d8d4f897de53104035
Tatsujin Chin <c910335@gmail.com>
2018-05-10 08:38:52 +0800

    support belongs_to custom method name (#186)

--- src/granite_orm/associations.cr
+++ src/granite_orm/associations.cr
@@ -1,24 +1,38 @@
 module Granite::ORM::Associations
   # define getter and setter for parent relationship
-  macro belongs_to(model_name)
-    belongs_to {{model_name}}, {{model_name.id}}_id : Int64
+  macro belongs_to(model)
+    {% if model.is_a? TypeDeclaration %}
+      belongs_to {{model.var}}, {{model.type}}, {{model.var}}_id : Int64
+    {% else %}
+      belongs_to {{model.id}}, {{model.id.camelcase}}, {{model.id}}_id : Int64
+    {% end %}
   end
 
-  macro belongs_to(model_name, decl)
-    field {{decl.var}} : {{decl.type}}
+  # ditto
+  macro belongs_to(model, foreign_key)
+    {% if model.is_a? TypeDeclaration %}
+      belongs_to {{model.var}}, {{model.type}}, {{foreign_key}}
+    {% else %}
+      belongs_to {{model.id}}, {{model.id.camelcase}}, {{foreign_key}}
+    {% end %}
+  end
+
+  # ditto
+  macro belongs_to(method_name, model_name, foreign_key)
+    field {{foreign_key}}
 
     # retrieve the parent relationship
-    def {{model_name.id}}
-      if parent = {{model_name.id.camelcase}}.find {{model_name.id}}_id
+    def {{method_name.id}}
+      if parent = {{model_name.id}}.find {{foreign_key.var}}
         parent
       else
-        {{model_name.id.camelcase}}.new
+        {{model_name.id}}.new
       end
     end
 
     # set the parent relationship
-    def {{model_name.id}}=(parent)
-      @{{model_name.id}}_id = parent.id
+    def {{method_name.id}}=(parent)
+      @{{foreign_key.var}} = parent.id
     end
   end
 
