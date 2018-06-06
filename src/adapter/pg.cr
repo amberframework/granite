455d7caecd033b6f34b486768946ff38e3d273a0
Charlie Hadden <charlie@hadden.me>
2018-06-06 02:49:49 +0100

    Fix last insert ID race condition (#220)
    * Add failing spec for ID race condition
    
    * Fix ID race condition in postgres
    
    * Fix ID race condition in mysql

--- src/adapter/pg.cr
+++ src/adapter/pg.cr
@@ -53,16 +53,18 @@ class Granite::Adapter::Pg < Granite::Adapter::Base
       stmt << ") VALUES ("
       stmt << fields.map { |name| "$#{fields.index(name).not_nil! + 1}" }.join(", ")
       stmt << ")"
+
+      stmt << " RETURNING #{quote(lastval)}" if lastval
     end
 
     log statement, params
 
     open do |db|
-      db.exec statement, params
       if lastval
-        return db.scalar(last_val()).as(Int64)
+        db.scalar(statement, params).as(Int32 | Int64).to_i64
       else
-        return -1_i64
+        db.exec statement, params
+        -1_i64
       end
     end
   end
@@ -111,10 +113,6 @@ class Granite::Adapter::Pg < Granite::Adapter::Base
     open do |db|
       db.exec statement, params
     end
-  end
-
-  private def last_val
-    return "SELECT LASTVAL()"
   end
 
   # This will update a row in the database.
