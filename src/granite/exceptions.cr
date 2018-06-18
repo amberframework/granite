f8e16f6086c7203fd73e4a61675f8d308ef4dd78
Thellior <Thellior@live.nl>
2018-06-18 21:59:01 +0200

    Removed Nil type because i fixed an bug than produced Nil values

--- src/granite/exceptions.cr
+++ src/granite/exceptions.cr
@@ -1,12 +1,12 @@
 module Granite
   class RecordInvalid < ::Exception
-    def initialize(class_name : String | Nil)
+    def initialize(class_name : String)
       super("Could not process #{class_name}")
     end
   end
 
   class RecordNotDestroyed < ::Exception
-    def initialize(class_name : String | Nil)
+    def initialize(class_name : String)
       super("Could not destroy #{class_name}")
     end
   end
