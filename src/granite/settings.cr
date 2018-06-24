b6fc2674b7e02287644c8bfa33304fde447177f4
Blacksmoke16 <yomoejoe@gmail.com>
2018-06-23 22:56:29 -0400

    Be a bit more future proof if more adapters were ever introduced

--- src/granite/settings.cr
+++ src/granite/settings.cr
@@ -2,7 +2,7 @@ require "logger"
 
 module Granite
   class Settings
-    class_property database_url = {"mysql" => "", "pg" => "", "sqlite" => ""}
+    class_property database_url = {} of String => String
     class_property logger = Logger.new STDOUT
 
     def initialize
