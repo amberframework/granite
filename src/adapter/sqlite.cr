13d5fc050c71e6109a84857def11542e4f638ebe
Erwin Kraan <Thellior@users.noreply.github.com>
2018-06-24 17:44:04 +0200

    Add ameba, fix style errors (#240)
    * Adding ameba
    
    * Review fixes

--- src/adapter/sqlite.cr
+++ src/adapter/sqlite.cr
@@ -52,7 +52,7 @@ class Granite::Adapter::Sqlite < Granite::Adapter::Base
       stmt << "INSERT INTO #{quote(table_name)} ("
       stmt << fields.map { |name| "#{quote(name)}" }.join(", ")
       stmt << ") VALUES ("
-      stmt << fields.map { |name| "?" }.join(", ")
+      stmt << fields.map { |_name| "?" }.join(", ")
       stmt << ")"
     end
 
