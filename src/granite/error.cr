57b759da52953d78bc000ad48431c36c4388b5d1
Blacksmoke16 <yomoejoe@gmail.com>
2018-07-17 16:50:55 -0400

    revert error.to_s to if/else

--- src/granite/error.cr
+++ src/granite/error.cr
@@ -4,7 +4,9 @@ class Granite::Error
   def initialize(@field : (String | Symbol | JSON::Any), @message : String? = "")
   end
 
-  def to_s
-    (@field == :base) ? @message : "#{@field.to_s.capitalize} #{message}"
+  if @field == :base
+    @message
+  else
+    "#{@field.to_s.capitalize} #{message}"
   end
 end
