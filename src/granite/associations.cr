ea9560aad1d9a2fb8e9bf3a0c7ca8492466d6381
Zaid Akram <hello@zaidakram.com>
2018-08-16 16:15:34 +0500

    Added simple has_one association with tests

--- src/granite/associations.cr
+++ src/granite/associations.cr
@@ -36,6 +36,24 @@ module Granite::Associations
     end
   end
 
+  macro has_one(model_name)
+    # retrieve the associated object
+    {% foreign_key = @type.stringify.split("::").last.underscore + "_id" %}
+    has_one {{model_name.id}}, foreign_key: {{foreign_key.id}}
+  end
+
+  macro has_one(model_name, foreign_key)
+    # retrieve the associated object
+    def {{model_name.id}}
+      {{model_name.id.camelcase}}.find_by({{foreign_key.id}}: self.id)
+    end
+
+    # set the associated object to given relationship
+    def {{model_name.id}}=(children)
+      children.{{foreign_key.id}} = self.id
+    end
+  end
+
   macro has_many(children_table)
     def {{children_table.id}}
       {% children_class = children_table.id[0...-1].camelcase %}
