08c953cf30a8db48418d999488ee657bfc697a72
Steven Jeffries <sjeffries@novillus.com>
2018-08-25 23:58:11 -0700

    Fixed trailing whitespaces.

--- src/granite/comparator.cr
+++ src/granite/comparator.cr
@@ -1,12 +1,11 @@
 # Adds equals, hash, and comparison methods to Granite::Base objects.
 module Granite::Comparator
-
   macro __process_comparator
 
     def_equals_and_hash {{ *FIELDS.keys }}
 
     # Compares this {{@type}} to another {{@type}} by comparing their fields in this order: {{ FIELDS.keys.join(", ").id }}.
-    # 
+    #
     # Each individual field will be compared using the <=> (if available).
     #
     # Returns 0 if the two objects are equal, a negative number if this object is considered less than other, or a positive number otherwise.
@@ -17,7 +16,7 @@ module Granite::Comparator
         elsif !self.{{field}} && other.{{field}}
           return 1
         elsif self.{{field}} != other.{{field}}
-          {% 
+          {%
             type = options[:type]
             type_node = options[:type].resolve
           %}
@@ -33,5 +32,4 @@ module Granite::Comparator
     include Comparable(self)
 
   end
-
 end
