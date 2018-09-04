6085b85bd329d2a1d1c2172aa6de98971d8e4617
Dru Jensen <drujensen@gmail.com>
2018-09-04 06:34:40 -0700

    do not try to remove plural  from association

--- src/granite/association_collection.cr
+++ src/granite/association_collection.cr
@@ -34,15 +34,15 @@ class Granite::AssociationCollection(Owner, Target)
   private getter through
 
   private def foreign_key
-    "#{Target.table_name}.#{Owner.table_name[0...-1]}_id"
+    "#{Target.table_name}.#{Owner.table_name}_id"
   end
 
   private def query
     if through.nil?
       "WHERE #{foreign_key} = ?"
     else
-      "JOIN #{through} ON #{through}.#{Target.table_name[0...-1]}_id = #{Target.table_name}.id " \
-      "WHERE #{through}.#{Owner.table_name[0...-1]}_id = ?"
+      "JOIN #{through} ON #{through}.#{Target.table_name}_id = #{Target.table_name}.id " \
+      "WHERE #{through}.#{Owner.table_name}_id = ?"
     end
   end
 end
