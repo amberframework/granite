7a1780732b42b86d635ddb9c597e574785690b64
Dru Jensen <drujensen@gmail.com>
2018-09-08 08:41:16 -0700

    support type declaration for has_one and has_many

--- src/granite/associations.cr
+++ src/granite/associations.cr
@@ -32,22 +32,34 @@ module Granite::Associations
   end
 
   macro has_one(model, **options)
-    {% class_name = options[:class_name] || model.id.camelcase %}
+    {% if model.is_a? TypeDeclaration %}
+      {% method_name = model.var %}
+      {% class_name = model.type %}
+    {% else %}
+      {% method_name = model.id %}
+      {% class_name = options[:class_name] || model.id.camelcase %}
+    {% end %}
     {% foreign_key = options[:foreign_key] || @type.stringify.split("::").last.underscore + "_id" %}
-    def {{model.id}}
+    def {{method_name}}
       {{class_name.id}}.find_by({{foreign_key.id}}: self.id)
     end
 
-    def {{model.id}}=(children)
+    def {{method_name}}=(children)
       children.{{foreign_key.id}} = self.id
     end
   end
 
   macro has_many(model, **options)
-    {% class_name = options[:class_name] || model.id.camelcase %}
+    {% if model.is_a? TypeDeclaration %}
+      {% method_name = model.var %}
+      {% class_name = model.type %}
+    {% else %}
+      {% method_name = model.id %}
+      {% class_name = options[:class_name] || model.id.camelcase %}
+    {% end %}
     {% foreign_key = options[:foreign_key] || @type.stringify.split("::").last.underscore + "_id" %}
     {% through = options[:through] %}
-    def {{model.id}}
+    def {{method_name.id}}
       Granite::AssociationCollection(self, {{class_name.id}}).new(self, {{foreign_key}}, {{through}})
     end
   end
