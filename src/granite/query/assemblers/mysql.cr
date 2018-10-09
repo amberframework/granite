2d571cc6832e4e1d94b10d4042fbcfe91f152982
Dru Jensen <drujensen@gmail.com>
2018-10-09 16:45:21 -0700

    reverse query builder
    Because SQL is fairly standard and the only differences are related to
    param replacement, this reverses the base class to contain the main code
    and moves the differences to the driver specific class.

--- src/granite/query/assemblers/mysql.cr
+++ src/granite/query/assemblers/mysql.cr
@@ -1,10 +1,10 @@
 # Query runner which finalizes a query and runs it.
 # This will likely require adapter specific subclassing :[.
 module Granite::Query::Assembler
-  class Postgresql(Model) < Base(Model)
+  class Mysql(Model) < Base(Model)
     def add_parameter(value : DB::Any) : String
       @numbered_parameters << value
-      "$#{@numbered_parameters.size}"
+      "?"
     end
   end
 end
