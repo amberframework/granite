ee4be8ba4a77dd7d8c1634dd23af78fd648c7b65
Dru Jensen <drujensen@gmail.com>
2018-10-14 21:19:58 -0700

    use string for class name

--- src/granite/query/builder_methods.cr
+++ src/granite/query/builder_methods.cr
@@ -1,24 +1,16 @@
-# DSL to be extended into a model
-# To activate, simply
-#
-# class Model < Granite::Base
-#   extend Query::BuilderMethods
-# end
 module Granite::Query::BuilderMethods
   def __builder
-    db_type = case adapter.class
-              when Granite::Adapter::Pg
+    db_type = case adapter.class.to_s
+              when "Granite::Adapter::Pg"
                 Granite::Query::Builder::DbType::Pg
-              when Granite::Adapter::Mysql
+              when "Granite::Adapter::Mysql"
                 Granite::Query::Builder::DbType::Mysql
-              when Granite::Adapter::Sqlite
-                Granite::Query::Builder::DbType::Sqlite
               else
-                raise "Adapter not supported #{adapter.class}"
+                Granite::Query::Builder::DbType::Sqlite
               end
 
     Builder(self).new(db_type)
   end
 
-  delegate where, count, order, offset, limit, first, to: __builder
+  delegate where, count, order, offset, limit, to: __builder
 end
