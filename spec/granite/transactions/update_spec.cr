75e8473063f1963e2d7090a01fca54dc2aca4448
Blacksmoke16 <yomoejoe@gmail.com>
2019-02-26 11:47:14 -0500

    Do not update created_at on update (#324)

--- spec/granite/transactions/update_spec.cr
+++ spec/granite/transactions/update_spec.cr
@@ -20,6 +20,27 @@ describe "#update" do
 
     Parent.find!(parent.id).name.should eq "New Parent"
   end
+
+  context "when created_at is nil" do
+    it "does not update created_at" do
+      parent = Parent.new(name: "New Parent")
+      parent.save!
+
+      created_at = parent.created_at!.at_beginning_of_second
+
+      # Simulating instantiating a new object with same ID
+      new_parent = Parent.new(name: "New New Parent")
+      new_parent.id = parent.id
+      new_parent.new_record = false
+      new_parent.updated_at = parent.updated_at
+      new_parent.save!
+
+      saved_parent = Parent.find!(parent.id)
+      saved_parent.name.should eq "New New Parent"
+      saved_parent.created_at.should eq created_at
+      saved_parent.updated_at.should eq Time.utc_now.at_beginning_of_second
+    end
+  end
 end
 
 describe "#update!" do
