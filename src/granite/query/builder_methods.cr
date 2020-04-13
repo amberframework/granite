723a982ded6938b6408c21f7544afd1cf0e2342b
Bruno Antunes <sardaukar.siet@gmail.com>
2020-04-13 20:26:54 +0100

    Adding #group_by to list of methods delegated to query builder

--- src/granite/query/builder_methods.cr
+++ src/granite/query/builder_methods.cr
@@ -12,5 +12,5 @@ module Granite::Query::BuilderMethods
     Builder(self).new(db_type)
   end
 
-  delegate where, order, offset, limit, to: __builder
+  delegate where, order, offset, limit, group_by, to: __builder
 end
