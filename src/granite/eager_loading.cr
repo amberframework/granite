9740a60cff379ccb7a26e03bd52109fb1672b959
Opak Alex <opak.alexandr@gmail.com>
2020-06-12 18:01:05 +0200

    fix

--- src/granite/eager_loading.cr
+++ src/granite/eager_loading.cr
@@ -1,5 +1,5 @@
 module Granite::EagerLoading
   def includes(value : Symbol)
-    Granite::Querying(self).new(self.select_container)
+    Granite::Querying(self).new(self.select_container, self.adapter, self.primary_name, self.quoted_table_name, self.name)
   end
 end
