be44117c7039501d40f099a85152827bb4b0856a
ionica21 <ionicat@windowslive.com>
2021-02-23 10:18:02 +1300

    Quote fields in PG query assembler

--- src/granite/query/assemblers/pg.cr
+++ src/granite/query/assemblers/pg.cr
@@ -4,6 +4,12 @@ module Granite::Query::Assembler
   class Pg(Model) < Base(Model)
     @placeholder = "$"
 
+    def field_list
+      # Override this method to quote the fields as upper case characters
+      # get converted to lower case in PG, which we do not want.
+      [Model.fields].flatten.map{ |field| "\"#{field}\"" }.join ", "
+    end
+
     def add_parameter(value : Granite::Columns::Type) : String
       @numbered_parameters << value
       "$#{@numbered_parameters.size}"
