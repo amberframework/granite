1ec0ea546b4cf8a9ac8da40f2880ad42c1320db6
Aurélien Delogu <aurelien.delogu@gmail.com>
2023-04-24 23:14:22 +0200

    Add context to RecordNotSaved/RecordNotDestroyed error messages (#452)

--- src/granite/exceptions.cr
+++ src/granite/exceptions.cr
@@ -2,20 +2,16 @@ module Granite
   class RecordNotSaved < ::Exception
     getter model : Granite::Base
 
-    def initialize(class_name : String, model : Granite::Base)
-      super("Could not process #{class_name}")
-
-      @model = model
+    def initialize(class_name : String, @model : Granite::Base)
+      super("Could not process #{class_name}: #{model.errors.first.message}")
     end
   end
 
   class RecordNotDestroyed < ::Exception
     getter model : Granite::Base
 
-    def initialize(class_name : String, model : Granite::Base)
-      super("Could not destroy #{class_name}")
-
-      @model = model
+    def initialize(class_name : String, @model : Granite::Base)
+      super("Could not destroy #{class_name}: #{model.errors.first.message}")
     end
   end
 end
