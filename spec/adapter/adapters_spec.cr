431ed8eb2ae5ba99b337e70051f965caf201bc46
Holden Omans <holden.omans@gmail.com>
2023-05-11 10:15:36 -0400

    spec updates

--- spec/adapter/adapters_spec.cr
+++ spec/adapter/adapters_spec.cr
@@ -1,8 +1,7 @@
 require "../spec_helper"
 
 class Foo < Granite::Base
-  connection sqlite
-
+  connection {{env("CURRENT_ADAPTER").id}}
   column id : Int64, primary: true
 end
 
@@ -13,47 +12,42 @@ end
 describe Granite::Connections do
   describe "registration" do
     it "should allow connections to be be saved and looked up" do
-      Granite::Connections.registered_connections.size.should eq 6
+      Granite::Connections.registered_connections.size.should eq 2
 
-      if connection = Granite::Connections["mysql"]
-        connection[:writer].url.should eq ENV["MYSQL_DATABASE_URL"]
+      if connection = Granite::Connections[CURRENT_ADAPTER]
+        connection[:writer].url.should eq ADAPTER_URL
       else
         connection.should_not be_falsey
       end
-      if connection = Granite::Connections["pg"]
-        connection[:writer].url.should eq ENV["PG_DATABASE_URL"]
-      else
-        connection.should_not be_falsey
-      end
-      if connection = Granite::Connections["sqlite"]
-        connection[:writer].url.should eq ENV["SQLITE_DATABASE_URL"]
-      else
-        connection.should_not be_falsey
-      end
-      if connection = Granite::Connections["sqlite_with_replica"]
-        connection[:writer].url.should eq ENV["SQLITE_DATABASE_URL"]
-        connection[:reader].url.should eq ENV["SQLITE_REPLICA_URL"]
-      else
-        connection.should_not be_falsey
+
+      case ENV["CURRENT_ADAPTER"]?
+      when "sqlite"
+        if connection = Granite::Connections["sqlite_with_replica"]
+          connection[:writer].url.should eq ENV["SQLITE_DATABASE_URL"]?
+          connection[:reader].url.should eq ADAPTER_REPLICA_URL
+        else
+          connection.should_not be_falsey
+        end
       end
     end
 
     it "should disallow multiple connections with the same name" do
-      expect_raises(Exception, "Adapter with name 'mysql' has already been registered.") do
-        Granite::Connections << Granite::Adapter::Pg.new(name: "mysql", url: ENV["PG_DATABASE_URL"])
+      Granite::Connections << Granite::Adapter::Pg.new(name: "mysql2", url: "mysql://localhost:3306/test")
+      expect_raises(Exception, "Adapter with name 'mysql2' has already been registered.") do
+        Granite::Connections << Granite::Adapter::Pg.new(name: "mysql2", url: "mysql://localhost:3306/test")
       end
     end
 
     it "should assign the correct connections to a model" do
       adapter = Foo.adapter
-      adapter.name.should eq "sqlite"
-      adapter.url.should eq ENV["SQLITE_DATABASE_URL"]
+      adapter.name.should eq CURRENT_ADAPTER
+      adapter.url.should eq ADAPTER_URL
     end
 
     it "should use the first registered connection if none are specified" do
       adapter = Bar.adapter
-      adapter.name.should eq "mysql"
-      adapter.url.should eq ENV["MYSQL_DATABASE_URL"]
+      adapter.name.should eq CURRENT_ADAPTER
+      adapter.url.should eq ADAPTER_URL
     end
   end
 end
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -2,20 +2,24 @@ require "mysql"
 require "pg"
 require "sqlite3"
 
-case ENV["CURRENT_ADAPTER"]?
+CURRENT_ADAPTER     = ENV["CURRENT_ADAPTER"]
+ADAPTER_URL         = ENV["#{CURRENT_ADAPTER.upcase}_DATABASE_URL"]
+ADAPTER_REPLICA_URL = ENV["#{CURRENT_ADAPTER.upcase}_REPLICA_URL"]? || ADAPTER_URL
+
+case CURRENT_ADAPTER
 when "pg"
-  Granite::Connections << Granite::Adapter::Pg.new(name: "pg", url: ENV["PG_DATABASE_URL"])
-  Granite::Connections.<<(name: "pg_with_replica", writer: ENV["PG_DATABASE_URL"], reader: ENV["PG_REPLICA_URL"], adapter_type: Granite::Adapter::Pg)
+  Granite::Connections << Granite::Adapter::Pg.new(name: CURRENT_ADAPTER, url: ADAPTER_URL)
+  Granite::Connections.<<(name: "pg_with_replica", writer: ADAPTER_URL, reader: ADAPTER_REPLICA_URL, adapter_type: Granite::Adapter::Pg)
 when "mysql"
-  Granite::Connections << Granite::Adapter::Mysql.new(name: "mysql", url: ENV["MYSQL_DATABASE_URL"])
-  Granite::Connections.<<(name: "mysql_with_replica", writer: ENV["MYSQL_DATABASE_URL"], reader: ENV["MYSQL_REPLICA_URL"], adapter_type: Granite::Adapter::Mysql)
+  Granite::Connections << Granite::Adapter::Mysql.new(name: CURRENT_ADAPTER, url: ADAPTER_URL)
+  Granite::Connections.<<(name: "mysql_with_replica", writer: ADAPTER_URL, reader: ADAPTER_REPLICA_URL, adapter_type: Granite::Adapter::Mysql)
 when "sqlite"
-  Granite::Connections << Granite::Adapter::Sqlite.new(name: "sqlite", url: ENV["SQLITE_DATABASE_URL"])
-  Granite::Connections.<<(name: "sqlite_with_replica", writer: ENV["SQLITE_DATABASE_URL"], reader: ENV["SQLITE_REPLICA_URL"], adapter_type: Granite::Adapter::Sqlite)
+  Granite::Connections << Granite::Adapter::Sqlite.new(name: CURRENT_ADAPTER, url: ADAPTER_URL)
+  Granite::Connections.<<(name: "sqlite_with_replica", writer: ADAPTER_URL, reader: ADAPTER_REPLICA_URL, adapter_type: Granite::Adapter::Sqlite)
 when Nil
   raise "Please set CURRENT_ADAPTER"
 else
-  raise "Unknown adapter #{ENV["CURRENT_ADAPTER"]}"
+  raise "Unknown adapter #{CURRENT_ADAPTER}"
 end
 
 require "spec"
