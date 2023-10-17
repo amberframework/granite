111ab84874a2ee16f4889ff5abae731e5fa07ca9
Jack Haward <jack.haward@bt.com>
2023-10-17 12:04:52 +0100

    update export.sh to dynamically export vars in .env file

--- export.sh
+++ export.sh
@@ -1,11 +1,10 @@
-export PG_DATABASE_URL=postgres://granite:password@localhost:5432/granite_db
-export PG_REPLICA_URL=postgres://granite:password@localhost:5432/granite__replica_db
-export MYSQL_DATABASE_URL=mysql://granite:password@localhost:3306/granite_db
-export MYSQL_REPLICA_URL=mysql://granite:password@localhost:3306/granite_replica_db
-export SQLITE_DATABASE_URL=sqlite3:./granite.db
-export SQLITE_REPLICA_URL=sqlite3:./granite_replica.db
-export PG_VERSION=15.2
-export MYSQL_VERSION=5.7
-export SQLITE_VERSION=3110000
-export SQLITE_VERSION_YEAR=2016
-export CURRENT_ADAPTER=sqlite
+#!/bin/bash
+
+if [ -f .env ]; then
+   while IFS= read -r line; do
+       export "$line"
+   done < .env
+   echo "Environment variables from .env file have been exported."
+else
+   echo "Error: The .env file does not exist."
+fi
