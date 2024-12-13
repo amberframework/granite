6e14d8ef90a6d7fdc1dc4340a6796b06c5de9811
Hugo Parente Lima <hugo.pl@gmail.com>
2024-12-13 10:38:12 -0300

    Update dependencies and fix version in shards.yml. (#508)
    * Fix version number.
    
    Needs tag v0.23.4 after merged.
    
    * Update dependencies.
    
    * User version number from shard.yml on VERSION constant.

--- spec/granite_spec.cr
+++ spec/granite_spec.cr
@@ -264,8 +264,7 @@ describe Granite::Base do
     describe "#to_yaml" do
       it "emits nil values when told" do
         t = TodoEmitNull.new(name: "test todo", priority: 20)
-        result = %(---\nid: \nname: test todo\npriority: 20\ncreated_at: \nupdated_at: \n)
-
+        result = %(---\nid:\nname: test todo\npriority: 20\ncreated_at:\nupdated_at:\n)
         t.to_yaml.should eq result
       end
 
